6a4366f78dd9054c960507b45f1b94c66cb4cda3
elReclusa <howell_j@hotmail.com>
2023-03-01 15:10:40 -0600

    setting up ant design layout- or at least trying to

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,93 @@
 import React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Button } from 'antd';
 
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import { Layout, Space } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />}
-              />
-              <Route 
-                path="/login" 
-                element={<Login />}
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />}
-              />
-              <Route 
-                path="/me" 
-                element={<Profile />}
-              />
-              <Route 
-                path="/profiles/:profileId"
-                element={<Profile />}
-              />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
-}
+    <div className="App">
+      <Button type="primary">Button</Button>
 
-export default App;
+
+    </div>
+  );
+ }
+
+ export default App;
+
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Home from './pages/Home';
+// import Profile from './pages/Profile';
+// import Signup from './pages/Signup';
+// import Login from './pages/Login';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+
+
+//     <ApolloProvider client={client}>
+//       <Router>
+//         <div className="flex-column justify-flex-start min-100-vh">
+//           <Header />
+//           <div className="container">
+//             <Routes>
+//               <Route 
+//                 path="/" 
+//                 element={<Home />}
+//               />
+//               <Route 
+//                 path="/login" 
+//                 element={<Login />}
+//               />
+//               <Route 
+//                 path="/signup" 
+//                 element={<Signup />}
+//               />
+//               <Route 
+//                 path="/me" 
+//                 element={<Profile />}
+//               />
+//               <Route 
+//                 path="/profiles/:profileId"
+//                 element={<Profile />}
+//               />
+//             </Routes>
+//           </div>
+//           <Footer />
+//         </div>
+//       </Router>
+//     </ApolloProvider>
+//   );
+// }
+
+
