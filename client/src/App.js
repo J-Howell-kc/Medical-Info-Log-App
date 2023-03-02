f6efe13b8cd428991ee4535951d8b8d826738908
elReclusa <howell_j@hotmail.com>
2023-03-01 18:07:12 -0600

    removed extra, unused layout styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,50 @@
 import React from 'react';
-import { Button } from 'antd';
-
 import { Layout, Space } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
-
-function App() {
-  return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-
-
-    </div>
-  );
- }
-
- export default App;
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
+const App = () => (
+  <Space
+    direction="vertical"
+    style={{
+      width: '100%',
+    }}
+    size={[0, 48]}
+  >
+    <Layout><Sider style={siderStyle}>Sider</Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
+  </Space>
+);
+export default App;
 
 // import {
 //   ApolloClient,
