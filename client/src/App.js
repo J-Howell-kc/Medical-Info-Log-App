40372607adc1eba8e929c86bf0178b26993278b4
elReclusa <howell_j@hotmail.com>
2023-03-01 19:55:43 -0600

    commented out some error-causing code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./app.css";
+// import "./app.css";
 import { Layout, Space } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 const headerStyle = {
