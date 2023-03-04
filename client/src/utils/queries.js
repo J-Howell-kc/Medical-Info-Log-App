db12f98254d39ae5ee4fccc730162d1ca1e24864
nathanielmuci <nate.muci@gmail.com>
2023-03-03 19:12:07 -0600

    added queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,135 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
 export const QUERY_ME = gql`
   query me {
     me {
       _id
-      name
-      skills
+      firstName
+      LastName
+      DOB
+      height
+      address
+      phone
+      allergies
+      weight {
+        _id
+        pounds
+        date
+      }
+      medication {
+        _id
+        medicationName
+        dosage
+        frequency
+        pillCount
+        startDate
+        endDate
+        taken
+      }
+      nutrition {
+        _id
+        food
+        drinks
+        calories
+        date
+      }
+      emergencyContact {
+        _id
+        firstName
+        lastName
+        address
+        phone
+        relationship
+      }
+      symptoms {
+        _id
+        description
+        startDate
+        endDate
+        intensity
+        duration
+        actionTaken
+      }
     }
   }
 `;
+
+export const QUERY_MEDICATION = gql`
+  query medication {
+    medication {
+      _id
+      medicationName
+      dosage
+      frequency
+      pillCount
+      startDate
+      endDate
+      taken
+      user {
+        _id
+      }
+  }
+}
+`;
+
+export const QUERY_NUTRITION = gql`
+  query nutrition {
+    nutrition {
+      _id
+      food
+      drinks
+      calories
+      date
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_EMERGENCYCONTACT = gql`
+  query emergencyContact {
+    emergencyContact {
+      _id
+      firstName
+      lastName
+      address
+      phone
+      relationship
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_SYMPTOMS = gql`
+  query symptoms {
+    symptoms {
+      _id
+      description
+      startDate
+      endDate
+      intensity
+      duration
+      actionTaken
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_WEIGHT = gql`
+  query weight {
+    weight {
+      _id
+      pounds
+      date
+      user {
+        _id
+      }
+    }
+  }
+`;
+
