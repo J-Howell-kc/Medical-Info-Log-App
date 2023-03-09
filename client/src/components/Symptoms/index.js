8cdef1e8214d7bb8532b2d2941cca3be7ad410f9
elReclusa <howell_j@hotmail.com>
2023-03-08 23:08:17 -0600

    reworked symptoms

--- client/src/components/Symptoms/index.js
+++ client/src/components/Symptoms/index.js
@@ -1,69 +1,104 @@
-import { useState } from "react";
+import { useReducer} from "react";
+import { useState} from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import { Form, Input, Button, DatePicker, InputNumber, Upload } from "antd";
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery} from "@apollo/client";
 import { ADD_SYMPTOMS } from "../../utils/mutations";
+import { QUERY_USER } from "../../utils/queries";
 
-const { RangePicker } = DatePicker;
+// const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 const Symptoms = () => {
+  const [symptoms, setSymptoms] = useState("");
 
-  const [form] = Form.useForm();
+  // const [form] = Form.useForm();
 const [addSymptoms] = useMutation(ADD_SYMPTOMS);
+const { loading, data } = useQuery(QUERY_USER);
+console.log(data)
 
-const onFinish = (values) => {
-addSymptoms({
-variables: {
-...values,
-severity: parseInt(values.severity),
-date: values.date.format("YYYY-MM-DD"),
-},
-});
-form.resetFields();
+const handleChange = (event) => {
+  console.log(event);
+  if (!event.target?.description  || !event.target?.symptom || !event.target?.date || !event.target?.datestartstop || !event.target?.intensity || !event.target?.actiontaken) {
+    return false;
+  }else {
+  const { symptom, date, datestartstop, description, intensity, actiontaken } = event.target;
+  
+
+  setSymptoms([
+    ...symptoms,
+    { description: description, symptom: symptom, date: date, datestartstop: datestartstop, intensity: intensity, actiontaken: actiontaken },
+  ]);
+}
 };
 
+const onFinish = async (event) => {
+  console.log(symptoms);
+
+  try {
+    const { data } = await addSymptoms({
+      variables: { ...symptoms },
+    });
+    console.log(data);
+  } catch (e) {
+    console.error(e);
+  }
+
+}
+// const onFinish = (values) => {
+// addSymptoms({
+// variables: {
+// ...values,
+// severity: parseInt(values.severity),
+// date: values.date.format("YYYY-MM-DD"),
+// },
+// });
+// form.resetFields();
+// };
+
   return (
+    
     <>
-      <Form
+    
+      <Form onValuesChange={handleChange} onFinish = {onFinish}
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 14 }}
         layout="horizontal"
         style={{ maxWidth: 600, marginTop: "50px" }}
       >
-        <Form.Item label="Symptom" labelWrap>
+        <Form.Item label="Symptom" name = "symptom" labelWrap>
           <Input placeholder = "Symptom" />
         </Form.Item>
 
-        <Form.Item label="Date">
+        <Form.Item label="Date" name = "date">
           <DatePicker />
         </Form.Item>
-        <Form.Item label="Date Start/Stop">
-          <RangePicker />
+        <Form.Item label="Date Start/Stop" name = "datestopstart">
+          <Input placeholder = "Enter start and stop dates here."/>
         </Form.Item>
 
-        <Form.Item label="Description" labelWrap>
+        <Form.Item label="Description" name = "description" labelWrap>
           <TextArea rows={8} placeholder = "Describe your symptom here."/>
         </Form.Item>
-        <Form.Item label="Intensity (1-10)">
+        <Form.Item label="Intensity (1-10)" name = "intensity">
           <InputNumber min={1} max={10} />
         </Form.Item>
-        <Form.Item label="Action taken:" labelWrap>
+        <Form.Item label="Action taken:" name = "actiontaken" labelWrap>
           <TextArea rows={8} placeholder = "Enter actions taken here."/>
         </Form.Item>
 
-        <Form.Item
+        {/* <Form.Item
           label="Photo"
           style={{ marginTop: "30px" }}
           valuePropName="fileList"
-        >
-          <Upload action="/upload.do" listType="picture-card">
+        > */}
+          {/* <Upload action="/upload.do" listType="picture-card">
             <div>
               <PlusOutlined />
               <div style={{ marginTop: 8 }}>Upload</div>
             </div>
           </Upload>
-        </Form.Item>
+        </Form.Item> */}
         <Form.Item label="Submit">
           <Button
             type="primary"
@@ -74,6 +109,7 @@ form.resetFields();
           </Button>
         </Form.Item>
       </Form>
+      
     </>
   );
 };
