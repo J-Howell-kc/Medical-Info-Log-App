fd839571bbd6c293de67894f24b59bb7ff10a395
nathanielmuci <nate.muci@gmail.com>
2023-03-08 20:05:25 -0600

    updated seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,6 +1,6 @@
 const db = require('../config/connection');
-const { Profile, User } = require('../models');
-const profileSeeds = require('./userSeeds.json');
+const { User } = require('../models');
+const userSeeds = require('./userSeeds.json');
 
 db.once('open', async () => {
   try {
